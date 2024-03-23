f91adceb738bfa09dd5e40b2e2bfabf5113a6915
Jesus Aguilar <aguilarandradejesus@gmail.com>
2024-03-23 17:42:06 -0400

    added some stuff

--- actions/actions.py
+++ actions/actions.py
@@ -30,13 +30,17 @@ class ActionTellTemperature(Action):
             msg = f"Can you give me a place."
             dispatcher.utter_message(text=msg)
             return[]
-        
-        owm = pyowm.OWM('3701e9245325560b2e1eb9a37bcdfce7')
-        weather_mgr = owm.weather_manager()
-        place = str(current_place) + ', US'
-        observation = weather_mgr.weather_at_place(place)
-        temperature = observation.weather.temperature("fahrenheit")["temp"]
-        
+        try:  
+            owm = pyowm.OWM('3701e9245325560b2e1eb9a37bcdfce7')
+            weather_mgr = owm.weather_manager()
+            place = str(current_place) + ', US'
+            observation = weather_mgr.weather_at_place(place)
+            temperature = observation.weather.temperature("fahrenheit")["temp"]
+        except:
+            print("Weather API Failed")
+            msg = f"You may have misspell the Location can you give me the location again."
+            dispatcher.utter_message(text=msg)
+            return []
         
         msg = f"The Temperature is {temperature} in " + str(current_place) + " now."
         dispatcher.utter_message(text=msg)
